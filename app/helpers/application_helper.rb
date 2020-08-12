d86de9dba5467eda1fa7692a9473f70210cbe5df
kaneko <ec2-user@ip-172-31-8-209.ap-northeast-1.compute.internal>
2020-08-12 06:17:27 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_name = "")
+    base_title = "AttendanceApp"
+    if page_name.empty?            # page_nameが空文字かどうか
+      base_title
+    else                           # 引数page_nameが空文字ではない場合
+      page_name + " | " + base_title  # 文字列を連結して返す
+    end
+  end
 end
